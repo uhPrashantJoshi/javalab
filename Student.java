ceda6545d80d47eaba212621d49f0b6202db2f06
Prashant R Joshi <87548699+uhPrashantJoshi@users.noreply.github.com>
2021-12-10 23:41:21 +0530

    Create Student.java
    Problem Statement 6 from the syllabus list

---
+++ Student.java
@@ -0,0 +1,17 @@
+/* Create a package CIE which has two classes- Student and Internals. The class Personal has
+members like usn, name, sem. The class internals has an array that stores the internal marks
+scored in five courses of the current semester of the student. Create another package SEE
+which has the class External which is a derived class of Student. This class has an array that
+stores the SEE marks scored in five courses of the current semester of the student. Import the
+two packages in a file that declares the final marks of n students in all five courses.
+*/
+
+package CIE;
+
+public class Student {
+
+    public String USN;
+    public String name;
+    public int semester;
+
+}
